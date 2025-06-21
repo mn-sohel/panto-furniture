85e272964dafb448b7401f2376766012080534aa
mn-sohel <sohel.m0087@gmail.com>
2025-06-21 12:22:28 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-red-600 font-["Poppins"] font-bold text-center p-20'>hellow</h1>
+    </>
+  )
+}
+
+export default App
