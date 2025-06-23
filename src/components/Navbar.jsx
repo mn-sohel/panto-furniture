a6dc0d7a0101095ebaf172e4c34886fd3413cac5
mn-sohel <sohel.m0087@gmail.com>
2025-06-23 13:17:35 +0600

    Navbar scroll part done
    episode 274

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FaTimes } from "react-icons/fa";
 import { FaBagShopping, FaBars } from "react-icons/fa6";
 import { Link, NavLink } from "react-router-dom";
@@ -32,17 +32,33 @@ const NavItems = ({toggleMenu}) => {
 };
 
 const Navbar = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false)
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
 
     const toggleMenu = () => {
         setIsMenuOpen(prevState => !prevState)
     }
+
+    // when scroll apply bg color to Navbar
+    useEffect(()=>{
+      const handleScroll = () => {
+        if(window.scrollY > 50){
+          setIsScrolled(true)
+        }else{
+          setIsScrolled(false)
+        }
+      }
+      window.addEventListener('scroll', handleScroll);
+      return () => {
+        window.addEventListener('scroll', handleScroll)
+      }
+    },[])
    
   return (
-    <header className={`fixed top-0 left-0 right-0 z-50 transition duration-300 ease-in-out text-white`}>
+    <header className={`fixed top-0 left-0 right-0 z-50 transition duration-300 ease-in-out  ${isScrolled ? "bg-white shadow-md text-black" : "bg-transparent text-white"}`}>
       <nav className="max-w-screen-2xl container mx-auto flex justify-between items-center py-6 px-4">
         {/* logo */}
-        <Link to="/" className="font-bold">Logo</Link>
+        <Link to="/" className="font-bold">Panto</Link>
 
         {/* hamburger menu for mobile */}
         <div onClick={toggleMenu} className="md:hidden text-xl cursor-pointer hover:text-[#E58411]">
