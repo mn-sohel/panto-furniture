4bad6af8b34c8afefde2ad90cd045e46f5c30705
mn-sohel <sohel.m0087@gmail.com>
2025-06-23 11:05:45 +0600

    banner blur effect done
    epsidode 271

--- src/pages/home/HeroPage.jsx
+++ src/pages/home/HeroPage.jsx
@@ -17,6 +17,24 @@ const HeroPage = () => {
                 </div>
             </div>
         </div>
+
+        {/* banner blur effect */}
+        <div className='absolute bg-gradient-to-t from-white via-transparent to-transparent inset-x-0 bottom-0 h-3/4 -mb-2 blur-sm'></div>
+
+        {/* hover button for tool tips */}
+        <div className='absolute bottom-24 left-24 group'>
+            <button className='relative p-4 bg-white/25 rounded-full border text-xl'>
+              <div className='hidden absolute bottom-full left-1/2 transform -translate-x-1/2 mb-3 p-2 rounded-lg bg-white/25 shadow-md group-hover:flex gap-2'>
+                <div className='size-6 bg-red-600 border border-white rounded-full'></div>
+                <div className='size-6 bg-blue-600 border border-white rounded-full'></div>
+                <div className='size-6 bg-green-600 border border-white rounded-full'></div>
+                
+                {/* showing tooltip arrow */}
+                <div className='absolute left-1/2 transform -translate-x-1/2 -bottom-2 w-0 h-0 border-l-8 border-l-transparent border-r-8 border-r-transparent border-t-8 border-t-white/55'></div>
+              </div>
+            </button>
+        </div>
+        
     </section>
   )
 }
